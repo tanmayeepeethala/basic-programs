8728cd2a958dedb4ec37a070a2a9edd42498be75
tanmayeepeethala <tanmayeepeethala09@gmail.com>
2026-01-28 22:07:52 +0530

    largest of 3 numbers

---
+++ 3.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+c=int(input("Enter c value:"))
+if a>b and a>c:
+	print("a is largest")
+elif b>c:
+	print("b is largest")
+else:
+	print("c is largest")
+	
